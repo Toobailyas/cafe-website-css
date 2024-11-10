207b0fd077c9e4f0cc083bac6b7275e0b5523ccc
Toobailyas <137196562+Toobailyas@users.noreply.github.com>
2024-11-10 21:13:44 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,4 @@ const nextConfig = {};
 
 export default nextConfig;
 
-const path = require('path');
-
-module.exports = {
-  experimental: {
-    esmExternals: false,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@fonts': path.resolve(__dirname, 'path-to-your-fonts-folder'),
-    };
-    return config;
-  },
-};
 
